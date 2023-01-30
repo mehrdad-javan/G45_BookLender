a2825bd59859e4133e2c6cb92af0de1e76965e6f
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-01-30 11:39:48 +0100

    part 5

---
+++ src/main/java/se/lexicon/g45_booklender/exception/DataNotFoundException.java
@@ -0,0 +1,9 @@
+package se.lexicon.g45_booklender.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+
+}
